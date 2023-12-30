6314b90be796e6b1a14e15a83a87919b6280a47d
Moses Makaka <moseskax@gmail.com>
2023-12-30 08:09:27 +0300

    Local server authentication fix

--- app.js
+++ app.js
@@ -1,40 +1,32 @@
-var express             = require("express"),
-    app                 = express(),
-    bodyParser          = require("body-parser"),
-    mongoose            = require("mongoose"),
-    Campground          = require("./models/campground"),
-    Comment             = require("./models/comment"),
-    flash               = require("connect-flash"),
-    seedDB              = require("./seeds"),
-    passport            = require("passport"),
-    LocalStrategy       = require("passport-local"),
+const express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    Campground = require("./models/campground"),
+    Comment = require("./models/comment"),
+    flash = require("connect-flash"),
+    seedDB = require("./seeds"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
     passportLocalMongoose = require("passport-local-mongoose"),
-    methodOverride      = require("method-override"),
-    User                = require("./models/user");
+    methodOverride = require("method-override"),
+    User = require("./models/user");
 
-require('dotenv').config(); // Load environment variables from .env file
 
-var indexRoutes         = require("./routes/index"),
-    campgroundRoutes    = require("./routes/campgrounds"),
-    commentRoutes       = require("./routes/comments");
+const indexRoutes = require("./routes/index"),
+    campgroundRoutes = require("./routes/campgrounds"),
+    commentRoutes = require("./routes/comments");
 
 mongoose.set('useUnifiedTopology', true);
-
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true }, function(err) {
-    if (err) {
-        console.error('Error connecting to MongoDB:', err);
-    } else {
-        console.log('Connected to MongoDB successfully');
-    }
-});
-
+mongoose.connect("mongodb+srv://moses:moses@cluster101.6utvxdo.mongodb.net/MyDatabase1", { useNewUrlParser: true, useUnifiedTopology: true });
 app.use(methodOverride("_method"));
 app.use(flash());
-//seedDB();  //to seed the DB
+
+// seedDB();  //to seed the DB
 
 // PASSPORT CONFIGURATION
 app.use(require("express-session")({
-    secret: process.env.SESSION_SECRET,
+    secret: "Kuldeep is the best",
     resave: false,
     saveUninitialized: false
 }));
@@ -45,21 +37,24 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
     res.locals.currentUser = req.user;
     res.locals.error = req.flash("error");
     res.locals.success = req.flash("success");
     next();
 });
 
+const PORT = process.env.PORT || 3000;
+const IP = process.env.IP || '127.0.0.1';
+
 app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("Server has been started...");
+app.listen(PORT, IP, function () {
+    console.log("Server imefunguliwa " + IP + ":" + PORT);
 });
